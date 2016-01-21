e7ede8b260f88f5dd7ff1322abe7014ba52e1252
root <root@gabmlserver>
2016-01-21 11:56:41 +0100

    undo voorbeeld

--- app/controllers/chatsessions_controller.rb
+++ app/controllers/chatsessions_controller.rb
@@ -1,6 +1,5 @@
 # class ChatsessionsController < ApplicationController
 #   def create
-#     #voorbeeld
 #     cookies.signed[:username] = params[:chatsession][:username]
 #     raise(params[:chatsession].inspect)
 #     redirect_to messages_path
