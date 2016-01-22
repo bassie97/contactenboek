a9e4585735c7216a28128de5afbac96e3d6cea7c
Bas de Ruiter <bderuiter@kwest-online.nl>
2016-01-22 15:09:25 +0100

    fixed faulty migrations

--- db/migrate/20151211123356_create_user_addresses.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserAddresses < ActiveRecord::Migration
-  def change
-    create_table :user_addresses do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20151214153909_drop_table_user_addresses.rb
+++
@@ -1,5 +0,0 @@
-class DropTableUserAddresses < ActiveRecord::Migration
-  def change
-    drop_table :user_addresses
-  end
-end
